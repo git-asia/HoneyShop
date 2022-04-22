5f84b2cf2aa7f5b21d1568bd5de0a60fc169718f
git-asia <tuszynska.joanna@gmail.com>
2022-04-22 12:28:42 +0200

    Initial commit

---
+++ src/admin.js
@@ -0,0 +1,74 @@
+const addProductForm = document.querySelector('.form-add-product');
+const inputName = document.querySelector('[name = "product-name"]');
+const inputPrice = document.querySelector('[name = "product-price"]');
+const productsList = document.querySelector('.products-list');
+
+// --------- Dodawanie nowo stworzonych produktów do localStorage -------
+// (ten sposób nie jest optymalny — lepsza była suma logiczna użyta w Baskecie — ale też często spotykany)
+
+const saveAddedProductToLS = (name, price) => {
+        // Pobieramy dotychczasową listę produktów
+        const productList = JSON.parse(localStorage.getItem('offered-products')) ?? [];
+        productList.push({name, price});
+        // Odsyłamy info do lS
+        localStorage.setItem('offered-products', JSON.stringify(productList))
+};
+
+// ----- Funkcja, która pozwoli na dodawanie nowych produktów do oferty na podstawie danych wejściowych z formularza ------
+const addProductToSalesOffer = (name, price) => {
+        // Tworzymy elementy listy
+        const newLi = document.createElement('li');
+        const newStrong = document.createElement('strong');
+        newStrong.innerText = name;
+        const newPriceTxt = document.createTextNode(` - ${price.toFixed(2)} zł `);
+        // Tworzymy przycisk i nadajemy mu klasę, atrybuty, treść i stylowanie zgodne z tymi w HTML
+        const newProductBtn = document.createElement('button');
+        newProductBtn.classList.add('.btn-buy')
+        newProductBtn.dataset.name = name
+        newProductBtn.dataset.price = String(price)
+        newProductBtn.innerText = 'Do koszyka'
+        newProductBtn.style.borderRadius='5px'
+        newProductBtn.style.padding ='5px'
+        newProductBtn.style.margin ='5px auto'
+        newProductBtn.addEventListener('click', addProductToBasket)
+        // Dodajemy poszczególne składowe do elementu newLi
+        newLi.appendChild(newStrong)
+        newLi.appendChild(newPriceTxt)
+        newLi.appendChild(newProductBtn)
+        // Dodajemy kompletne newLi do listy produktów
+        productsList.append(newLi);
+}
+// Pobieramy dane z lS
+const loadAddedProductFromLS = () => {
+        const productList = JSON.parse(localStorage.getItem('offered-products')) ?? [];
+        // destrukturyzacja od razu wstawiona do zmiennej
+        for (const {name, price} of productList) {
+                addProductToSalesOffer(name, price)
+        }
+}
+// Czyścimy inputy
+const clearData = () => {
+        inputName.value = ''
+        inputPrice.value = ''
+}
+
+// -------- Obsługa wysyłania formularza do dodawania produktów -------------
+const handleAddProductFormSubmit = event => {
+        // spowoduje, że przeglądarka nie przeładuje str.po submicie
+        event.preventDefault();
+        // wartość inputów formularza wyciągamy dopiero w momencie jego przesłania; żeby dostać się do wartości przekazanej przez użytkownika używamy właściwości /.value/
+        const nameFromInput = inputName.value
+        const priceFromInput = Number(inputPrice.value)
+
+        // Wywołujemy callbacki
+        clearData()
+        addProductToSalesOffer(nameFromInput, priceFromInput)
+        saveAddedProductToLS(nameFromInput, priceFromInput);
+
+}
+
+// aEL na submit nie klik, żeby obsłużyć też Enter
+addProductForm.addEventListener('submit', handleAddProductFormSubmit)
+
+// Wywołujemy funkcję
+loadAddedProductFromLS()
---
+++ src/basket.js
@@ -0,0 +1,104 @@
+class Basket {
+    // Początkowo tworzymy pusty koszyk — tablicę obiektów, kt docelowo będzie zawierała przechowywane w koszyku produkty.
+    // Po dodaniu opcji localStorage modyfikujemy nasz konstruktor, który teraz będzie zawierał odniesienie do metody zwracającej dane z lS.
+    //! Konstruktory działają synchronicznie – dopóki nie dostaniemy danych z lS, kod nie pójdzie dalej.
+    constructor(){
+        // W konstruktorze możemy tworzyć zmienne (np. dla zwiększenia czytelności kodu)
+        const lSLoad = this.loadFromLocalStorage()
+        // Operator trójargumentowy: jeśli lSL zwróci jakąś wartość (T), to wykonaj metodę lFLS(), jeśli zwróci null (F), stwórz pustą tablicę.
+        this.items = lSLoad ? lSLoad : [];
+
+        // Jeszcze krótsze wersje (przy tych zapisach usuwamy const lSLoad, bo jej wartość wstawiliśmy bezpośrednio do wyrażenia)
+        // 1. suma logiczna: jeśli lSL == F, to zrób [], a jeśli T, to wywołaj lSL;
+        //this.items = this.loadFromLocalStorage() || []
+
+        // 2. nullish operator: jeśli lSL == null or undefined, to zrób [], a jeśli T, to wywołaj lSL;
+        //this.items = this.loadFromLocalStorage() ?? []
+
+        //Powyższe zapisy dokładnie odpowiada temu:
+            // if (lSLoad) {
+            //     this.items = lSLoad
+            // } else {
+            //     this.items = []
+            // }       //
+    }
+    // Koszyk możemy wyczyścić...
+    clear() {
+      this.items.length = 0;
+      this.saveToLocalStorage()
+    }
+    //...lub coś do niego dodać
+    add(item) {
+        this.items.push(item);
+        this.saveToLocalStorage()
+    }
+
+    // Zliczamy wartość cenową produktów, które mamy w koszyku
+    getTotal() {
+        return this.items.reduce((prev, product) => prev + product.price, 0);
+    }
+    // Info co mamy w koszyku oraz ile kosztują poszczególne produkty
+    getBasketSummary() {
+        return this.items
+            .map((product, i) => {
+                return {
+                    id: i + 1,
+                    text: `${i + 1} - ${product.name} - ${product.price.toFixed(2)} zł`
+                }
+            });
+    }
+    // Możemy usunąć wybrany produkt z koszyka
+    remove(number) {
+      this.items.splice(number - 1, 1);
+      this.saveToLocalStorage()
+    }
+
+    // Przechowywanie zawartości koszyka w localStorage (musimy zmienić obiekt na stringa).
+    // Wywołujemy w każdym miejscu, gdzie koszyk ulega zmianie.
+    saveToLocalStorage() {
+        localStorage.setItem('basket-items', JSON.stringify(this.items))
+    }
+    // Odczytywanie (get) i pobieranie (return) wartości przechowywanych w localStorage — musimy z powrotem zmienić stringa na obiekt (parse) lub tworzymy pustą tablicę, jeśli lS nie przechowuje żadnych wartości.
+    // Wywołujemy w konstruktorze Basketa.
+
+    loadFromLocalStorage() {
+        return JSON.parse(localStorage.getItem('basket-items'))
+                //// inna prosta acz długa wersja
+                // const jsonItem = localStorage.getItem('basket-items')
+                // if (jsonItem === null) {
+                //     return[]
+                // } else {
+                //     return JSON.parse(jsonItem)
+                // }
+    }
+}
+
+class Product {
+    constructor(productName, productPrice) {
+      this.name = productName;
+      this.price = productPrice;
+}
+// Zmiana ceny produktu-funkcja stworzona 'na zapas' na razie nieużywana
+//     setNewPrice(newPrice) {
+//          this.price = newPrice
+//     }
+}
+
+
+
+
+
+// // Sprawdzanie kodu
+// const shopBasket = new Basket();
+// const oranges = new Product('Pomarańcze luz', 6.99)
+// const cucumbers = new Product('Ogórki gruntowe', 3)
+//
+// shopBasket.add(cucumbers)
+// shopBasket.add(cucumbers)
+// shopBasket.add(oranges)
+//
+// shopBasket.remove(2)
+// shopBasket.add(cucumbers)
+// shopBasket.add(oranges)
+// shopBasket.showBasket()
+// console.log(shopBasket.getTotal())
